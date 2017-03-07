85dd033d3616485a96f239633197c5fadc6fbee2
Luis Del Águila <luisdelaguila@outlook.com>
2017-03-06 21:48:36 -0600

    Replace twitter handle link for Github star badge

--- pages/grid.js
+++ pages/grid.js
@@ -6,6 +6,10 @@ const stylesMap = require('../styles-map')
 module.exports = (state, prev, send) => {
   return html`
     <div>
+      <div class="header">
+        <h2>Go ahead, click it!</h2>
+      </div>
+
       <table class="grid">
         ${state.grid.map((row, m) => html`
           <tr>
@@ -25,7 +29,7 @@ module.exports = (state, prev, send) => {
       </table>
 
       <div class="footer">
-        <a href="https://twitter.com/delaguilaluis">@delaguilaluis</a>
+        <iframe src="https://ghbtns.com/github-btn.html?user=delaguilaluis&repo=patchwork&type=star&count=true" frameborder="0" scrolling="0" width="80px" height="20px"></iframe>
       </div>
     </div>
   `
