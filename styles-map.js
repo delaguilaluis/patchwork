7b6b451e145cc7f9c3717707a3487ad9fd71a6aa
Luis Del Águila <luisdelaguila@outlook.com>
2017-03-07 20:56:47 -0600

    fix(pointed borders) - Change the appearance order and style of ‘pointed’ borders to be consistent with the rounded ones
    Also rename them to ‘flat’ borders

--- styles-map.js
+++ styles-map.js
@@ -7,8 +7,8 @@ module.exports = {
   3: 'box top-right',
   4: 'box bottom-right',
   5: 'box bottom-left',
-  6: 'pointed-border top-left',
-  7: 'pointed-border top-right',
-  8: 'pointed-border bottom-right',
-  9: 'pointed-border bottom-left'
+  6: 'flat-border top-left',
+  7: 'flat-border top-right',
+  8: 'flat-border bottom-right',
+  9: 'flat-border bottom-left'
 }
