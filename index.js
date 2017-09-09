c822367c4da8cf8b8a887478495a29fefc153fcd
Luis Del Águila <luisdelaguila@outlook.com>
2017-09-09 15:22:34 -0600

    refactor(framework) - Update choo to from v4 to v6

--- index.js
+++ index.js
@@ -1,67 +1,60 @@
 'use strict'
 
 const choo = require('choo')
-const xtend = require('xtend')
 const css = require('sheetify')
 const styles = require('./catalogs/styles')
+
 css('css-wipe')
 css('tachyons')
 css('./styles.css')
+
 const app = choo()
 
-const width = 8
-const height = 8
-const grid = []
-const stylesLength = Object.keys(styles).length
+app.use(patchwork)
+app.route('/', require('./views/main'))
+app.mount('body')
 
-for (let m = 0; m < height; m += 1) {
-  const column = []
-  for (let n = 0; n < width; n += 1) {
-    column.push({
-      element: '',
-      styleID: 0,
-      colorID: 0
-    })
-  }
+function patchwork (state, emitter) {
+  state.grid = makeGrid(8, 8)
+  state.colorPicker = { selectionID: 0 }
 
-  grid.push(column)
+  emitter.on('changeStyle', function ({ m, n, colorID }) {
+    const stylesLength = Object.keys(styles).length
+    const nextID = state.grid[m][n].styleID + 1
+
+    if (nextID === stylesLength) { // Cycle
+      state.grid[m][n].styleID = 0
+    } else {
+      state.grid[m][n].styleID += 1
+    }
+
+    state.grid[m][n].colorID = state.colorPicker.selectionID
+
+    emitter.emit('render')
+  })
+
+  emitter.on('changeColor', function ({ selectionID }) {
+    state.colorPicker.selectionID = selectionID
+
+    emitter.emit('render')
+  })
 }
 
-app.model({
-  state: {
-    grid,
-    colorPicker: {
-      selectionID: 0
+function makeGrid (width, height) {
+  const grid = []
+
+  for (let m = 0; m < height; m += 1) {
+    const column = []
+    for (let n = 0; n < width; n += 1) {
+      column.push({
+        element: '',
+        styleID: 0,
+        colorID: 0
+      })
     }
-  },
-  reducers: {
-    changeStyle: (state, { m, n, colorID }) => {
-      const newState = xtend(state)
-      const nextID = newState.grid[m][n].styleID + 1
 
-      if (nextID === stylesLength) {
-        newState.grid[m][n].styleID = 0
-      } else {
-        newState.grid[m][n].styleID += 1
-      }
-
-      newState.grid[m][n].colorID = state.colorPicker.selectionID
-
-      return newState
-    },
-
-    changeColor: (state, { selectionID }) => {
-      const newState = xtend(state)
-      newState.colorPicker.selectionID = selectionID
-
-      return newState
-    }
+    grid.push(column)
   }
-})
 
-app.router([
-  ['/', require('./pages/main')]
-])
-
-const tree = app.start()
-document.body.appendChild(tree)
+  return grid
+}
